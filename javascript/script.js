498b167191b1fe7a77145e5c23f3108f6be1c48f
HarukoRakuke <bettytsimaylo@gmail.com>
2024-05-25 19:09:26 +0300

    fixed_version_of_posters

--- javascript/script.js
+++ javascript/script.js
@@ -1,64 +1,108 @@
 let assets = document.querySelectorAll('.asset');
+let buttons = document.querySelectorAll('h1');
+let section = document.querySelector('section');
 
-function postersSpawn() {
-  function posterSerie(assets, i) {
+function assetClick() {
+  assets.forEach((asset, i) => {
+    asset.addEventListener('click', () => {
+      posterView(i);
+    });
+  });
+
+  function posterView(i) {
     let posters = assets[i].querySelectorAll('.poster');
-    posters.forEach((poster, index) => {
-      poster.style.backgroundImage = `url('/img/asset${i + 1}_${
-        index + 1
-      }.png')`;
+    posters.forEach((poster) => {
+      poster.style.position = 'relative';
+      poster.style.animation = '';
+      poster.style.scale = '1';
+      assets[i].style.display = 'flex';
+      section.style.paddingRight = '0';
+      assets[i].style.width = '80vw';
+      assets[i].style.overflowX = 'scroll';
     });
   }
-  assets.forEach((asset, i) => {
-    posterSerie(assets, i);
-  });
-}
-postersSpawn();
-
-function posterGenerate(i) {
-  let section = document.querySelector('section');
-  section.style.paddingRight = '20vw';
-  assets.forEach((asset) => {
-    asset.style.display = 'none';
-    asset.style.width = '';
-    asset.style.overflowX = '';
-  });
-  assets[i].style.display = 'block';
-  let posters = assets[i].querySelectorAll('.poster');
-  posters.forEach((poster, index) => {
-    poster.style.display = 'none';
-    poster.style.position = 'absolute';
-    poster.style.animation = '';
-  });
-
-  posters.forEach((poster, index) => {
-    setTimeout(() => {
-      poster.style.display = 'block';
-      poster.style.animation = `scale 5s linear forwards`;
-    }, index * 1000);
-  });
 }
 
-assets.forEach((asset) => {
-  asset.addEventListener('click', () => scrollPosters(asset));
-});
+function buttonHover() {
+  buttons.forEach((button, i) => {
+    button.addEventListener('mouseover', () => {
+      section.style.paddingRight = '20vw';
+      let asset = assets[i];
+      assets.forEach((serie) => {
+        serie.style.display = 'none';
+        serie.style.width = '';
+      });
+      asset.style.display = 'block';
 
-function scrollPosters(asset) {
-  let section = document.querySelector('section');
-  section.style.paddingRight = '0';
-  asset.querySelectorAll('.poster').forEach((poster) => {
-    asset.style.width = '80vw';
-    asset.style.overflowX = 'scroll';
-    asset.style.display = 'flex';
-    poster.style.position = 'relative';
+      function removePostersFromOtherAssets() {
+        assets.forEach((serie, index) => {
+          if (index !== i) {
+            let excessPosters = serie.querySelectorAll('.poster');
+            excessPosters.forEach((poster) => {
+              poster.parentNode.removeChild(poster);
+            });
+          }
+        });
+      }
+
+      removePostersFromOtherAssets();
+
+      let index;
+      let posters;
+      let assetlength;
+
+      function appendPoster(i, index) {
+        let p = document.createElement('div');
+        p.classList.add('poster');
+        p.style.backgroundImage = `url('/img/asset${i + 1}_${index + 1}.png')`;
+        p.style.display = 'block';
+        p.style.animation = `2s scale linear forwards`;
+        p.style.animationDelay = `${index - 1}s`;
+        asset.appendChild(p);
+      }
+
+      function generatePoster(i, index, assetlength) {
+        for (index = 0; index < assetlength; index++) {
+          appendPoster(i, index);
+          posters = asset.querySelectorAll('.poster');
+          let posterstoRemove = Math.max(0, posters.length - assetlength + 1);
+          if (posters.length > assetlength) {
+            for (let j = 0; j < posterstoRemove; j++) {
+              posters[j].parentNode.removeChild(posters[j]);
+            }
+          }
+        }
+      }
+
+      if (i === 0) {
+        assetlength = 5;
+        generatePoster(i, index, assetlength);
+      }
+      if (i === 1) {
+        assetlength = 8;
+        generatePoster(i, index, assetlength);
+      }
+      if (i === 2) {
+        assetlength = 5;
+        generatePoster(i, index, assetlength);
+      }
+      if (i === 3) {
+        assetlength = 3;
+        generatePoster(i, index, assetlength);
+      }
+      if (i === 4) {
+        assetlength = 5;
+        generatePoster(i, index, assetlength);
+      }
+      if (i === 5) {
+        assetlength = 4;
+        generatePoster(i, index, assetlength);
+      }
+    });
   });
 }
 
-let buttons = document.querySelectorAll('h1');
-buttons.forEach((button, i) => {
-  button.addEventListener('mouseover', () => posterGenerate(i));
-  button.addEventListener('click', () => {
-    let asset = assets[i];
-    scrollPosters(asset);
-  });
+document.addEventListener('DOMContentLoaded', () => {
+  buttonHover();
+  assetClick();
 });
