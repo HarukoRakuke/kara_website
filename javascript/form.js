98e485673158c68a54536d8c9db7dff820936185
HarukoRakuke <bettytsimaylo@gmail.com>
2024-06-23 09:42:16 +0300

    added_form

---
+++ javascript/form.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let formDates = document.querySelectorAll('.date');
+  let reasons = document.querySelectorAll('.reason');
+  let details = document.querySelector('.details');
+  let name = document.querySelector('.name');
+  let overlapSection = document.querySelector('form');
+  let closeButton = document.querySelector('.closeForm');
+  let openButton = document.querySelector('.openButton');
+
+  function changeOpacity() {
+    formDates.forEach((formDate) => {
+      formDate.addEventListener('change', () => {
+        formDate.style.opacity = '1';
+      });
+      details.addEventListener('mouseover', () => {
+        details.removeAttribute('placeholder');
+      });
+      details.addEventListener('mouseout', () => {
+        details.setAttribute(
+          'placeholder',
+          'ваши воспоминания и последующие наблюдения, которые могут помочь в расследовании'
+        );
+      });
+      name.addEventListener('mouseover', () => {
+        name.removeAttribute('placeholder');
+      });
+      name.addEventListener('mouseout', () => {
+        name.setAttribute('placeholder', 'прижизненное имя и фамилия');
+      });
+    });
+    reasons.forEach((reason) => {
+      reason.addEventListener('click', () => {
+        if (reason.className == 'reason selected') {
+          reason.classList.remove('selected');
+        } else {
+          reason.classList.add('selected');
+        }
+      });
+    });
+  }
+
+  function popup() {
+    overlapSection.style.zIndex = '11';
+    overlapSection.style.opacity = '1';
+    closeSee();
+  }
+
+  function openPopup() {
+    openButton.addEventListener('click', () => {
+      popup();
+    });
+  }
+
+  function closeSee() {
+    if (overlapSection.style.zIndex == '-1') {
+      closeButton.style.display = 'none';
+    } else if (overlapSection.style.zIndex == '11') {
+      closeButton.style.display = 'block';
+    }
+  }
+
+  function closePopup() {
+    closeButton.addEventListener('click', () => {
+      overlapSection.style.zIndex = '-1';
+      overlapSection.style.opacity = '0';
+      closeButton.style.display = 'none';
+    });
+  }
+
+  changeOpacity();
+  openPopup();
+  closePopup();
+});
